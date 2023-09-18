1a411dbbc1afcc8d1329cf37e752653409b32c8d
Sarah Francis <sarah.katherinefrancis@gmail.com>
2023-09-18 15:34:55 -0400

    Redux integration and Counter component

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,21 @@
-import classes from './Counter.module.css';
+// Import the useSelector hook from react-redux to access data from the Redux store.
+import { useSelector } from "react-redux";
 
+import classes from "./Counter.module.css";
+
+// Define the Counter component.
 const Counter = () => {
+  // Use useSelector to select and retrieve the 'counter' state from the Redux store.
+  const counter = useSelector((state) => state.counter);
+
+  // Placeholder function for a button click event handler (not implemented in this code).
   const toggleCounterHandler = () => {};
 
+  // Render the Counter component, displaying the current counter value and a button.
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+// Import the Provider component from react-redux to enable Redux integration.
+import { Provider } from "react-redux";
 
-import './index.css';
-import App from './App';
+// Import the Redux store created in "./store/index".
+import store from "./store/index";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+import "./index.css";
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// Render the App component wrapped in the Redux Provider.
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
