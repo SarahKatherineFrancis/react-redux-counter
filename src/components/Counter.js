d1707b029cee0308bc717609e2d11409baeacfd3
Sarah Francis <sarah.katherinefrancis@gmail.com>
2023-09-18 15:42:11 -0400

    Enhance Counter with dispatch and handlers

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,21 +1,39 @@
-// Import the useSelector hook from react-redux to access data from the Redux store.
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 
 import classes from "./Counter.module.css";
 
 // Define the Counter component.
 const Counter = () => {
+  // Create a dispatch function to dispatch actions to the Redux store.
+  const dispatch = useDispatch();
+
   // Use useSelector to select and retrieve the 'counter' state from the Redux store.
   const counter = useSelector((state) => state.counter);
+
+  // Define an event handler to increment the counter.
+  const incrementHandler = () => {
+    // Dispatch an 'increment' action to the Redux store.
+    dispatch({ type: "increment" });
+  };
+
+  // Define an event handler to decrement the counter.
+  const decrementHandler = () => {
+    // Dispatch a 'decrement' action to the Redux store.
+    dispatch({ type: "decrement" });
+  };
 
   // Placeholder function for a button click event handler (not implemented in this code).
   const toggleCounterHandler = () => {};
 
-  // Render the Counter component, displaying the current counter value and a button.
+  // Render the Counter component, displaying the current counter value and buttons for incrementing and decrementing.
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
